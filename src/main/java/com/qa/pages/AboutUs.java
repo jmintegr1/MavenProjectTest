dbc5ba46462759b6eb9828ec8c5870180d2333d1
jmintegr1 <30910806+jmintegr1@users.noreply.github.com>
2019-09-05 14:55:43 -0400

    contactInfo
    Contact Info and business logic

--- src/main/java/com/qa/pages/AboutUs.java
+++ src/main/java/com/qa/pages/AboutUs.java
@@ -5,5 +5,7 @@ public class AboutUs {
 	public void aboutUs() {
 		System.out.println("About Us Page");
 	}
-
+	
+	public void contactInfo(){
+		System.out.println("Call us so we can serve you");
 }
