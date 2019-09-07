c5971acc6208d6cefb20822f291d593fe2597576
jmintegr1 <jmintegr1@gmail.com>
2019-09-07 09:45:14 -0400

    History of Us

---
+++ src/test/java/com/qa/tests/AboutUsTests.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author jewellmehedi
+ *
+ */
+public class AboutUsTests {
+
+
+	@Test
+	public void aboutUsTests() {
+		System.out.println("History of us and why you should do business with us");
+	}
+
+}
