cc6b27619fb494e2d2e5a7189c9fc6bf63401864
jmintegr1 <30910806+jmintegr1@users.noreply.github.com>
2019-09-07 09:28:30 -0400

    ourCoreValues

--- src/main/java/com/qa/pages/AboutUs.java
+++ src/main/java/com/qa/pages/AboutUs.java
@@ -6,6 +6,10 @@ public class AboutUs {
 		System.out.println("About Us Page");
 	}
 	
+	public void ourCoreValues(){
+		System.out.println("Facing Fear = Courage, Truth, Justice, Self Control, and hard work!!");
+	}
+	
 	public void contactInfo(){
 		System.out.println("Call us so we can serve you");
 }
